3103e2905ffc11eac2238bf88f94fac9dc319b21
WKpeKepW <dan9o5ru@gmail.com>
2023-11-02 06:24:34 +0400

    Working aggregate

--- main.py
+++ main.py
@@ -2,28 +2,49 @@ from mpsiemlib.common import Settings, Creds, MPSIEMAuth
 from mpsiemlib.modules import Filters, EventsAPI
 import time
 
-def get_credsTxt():
-    with open('creds.txt','r') as txt:
-        return txt.read()
-
-if __name__ == '__main__':
-    
-    pass
-
 
 def default():
-    creds = Creds()
-    creds.core_hostname = '172.17.250.66'
-    creds.core_login = 'vakulik-dv'
-    creds.core_auth_type = 0
-    creds.core_pass = get_credsTxt()
-    auth = MPSIEMAuth(creds, Settings())
-    auth.get_session()
-    events = EventsAPI(auth, Settings())
-    filters = Filters(auth, Settings())
-    filter_list = filters.get_filters_list()
+    ini = Initialize()
+    filter_list = ini.filters.get_filters_list()
+
     for uuid_filter in filter_list:
-        if filter_list[uuid_filter]['name'] == '[malware] KES':
-            print(filters[uuid_filter])
-    filters.close()
-    events.close() 
+        if filter_list[uuid_filter]['name'] == 'src_distinct_dst_SMB_bot':
+            #print(ini.filters.get_filter_info(uuid_filter)['query']['aggregate'])
+            #print(ini.filters.get_filter_info(uuid_filter)['query']['select'])
+            print(ini.events.get_events_by_filter_aggregation(
+                filter=ini.filters.get_filter_info(uuid_filter)['query']['where'],
+                fields=ini.filters.get_filter_info(uuid_filter)['query']['select'],
+                groupBy=ini.filters.get_filter_info(uuid_filter)['query']['group'],
+                aggregateBy=ini.filters.get_filter_info(uuid_filter)['query']['aggregate'],
+                time_from=time.time()-3600,
+                time_to=time.time(),
+                limit=10,
+                offset=0
+            ))
+
+    ini.close()
+
+class Initialize:
+    def close(self):
+        self.filters.close()
+        self.events.close()
+
+    def get_credsTxt(self):
+        with open('creds.txt','r') as txt:
+            return txt.read()
+        
+    def __init__(self) -> None:
+        creds = Creds()
+        creds.core_hostname = '10.81.41.5'
+        creds.core_login = 'vols_3'
+        creds.core_auth_type = 0
+        creds.core_pass = self.get_credsTxt()
+        auth = MPSIEMAuth(creds, Settings())
+        auth.get_session()
+        self.events = EventsAPI(auth, Settings())
+        self.filters = Filters(auth, Settings())
+        
+        
+if __name__ == '__main__':
+    default()
+    pass
--- mpsiemlib/modules/EventsAPI.py
+++ mpsiemlib/modules/EventsAPI.py
@@ -10,6 +10,7 @@ class EventsAPI(ModuleInterface, LoggingHandler):
     __api_event_details = "/api/events/v2/events/{}/normalized?time={}"
     __api_events = "/api/events/v2/events?limit={}&offset={}"
     __api_events_for_incident = "/api/events/v2/events/?incidentId={}&limit={}&offset={}"
+    __api_events_aggregate = "/api/events/v2/events/aggregation?limit={}&offset={}"
 
     def __init__(self, auth: MPSIEMAuth, settings: Settings):
         ModuleInterface.__init__(self, auth, settings)
@@ -105,10 +106,9 @@ class EventsAPI(ModuleInterface, LoggingHandler):
             raise Exception("Core data request return None or has wrong response structure")
         return response.get("events")
 
-
     def get_count_events_by_filter(self, filter, fields, time_from, time_to, limit, offset) -> dict:
         """
-        Получить события по фильру
+        Получить количество событий по фильру
 
         Args:
             filter : фильтр на языке PDQL
@@ -155,6 +155,65 @@ class EventsAPI(ModuleInterface, LoggingHandler):
                            'hostname="{}"'.format(self.__core_hostname))
             raise Exception("Core data request return None or has wrong response structure")
         return response.get("totalCount")
+    
+    def get_events_by_filter_aggregation(self, filter, fields, groupBy, aggregateBy, time_from, time_to, limit, offset) -> dict:
+        """
+        Получить агрегированные события по фильру
+
+        Args:
+            filter : фильтр на языке PDQL
+            fields : список запрашиваемых полей событий
+            time_from : начало диапазона поиска (Unix timestamp в секундах)
+            aggregateBy : правила агрегации
+            groupBy : поле группировки
+            time_to : конец диапазона поиска (Unix timestamp в секундах)
+            limit: число запрашиваемых событий, соответсвующих фильтру
+            offset: позиция, начиная с которой возвращать требуемое число событий, соответсвующих фильтру
+        Returns:
+            [type]: массив событий
+        Example_aggregateBy:
+            {"function":"COUNT","field":"dst.ip","unique":true}
+        Example_groupBy:
+            ["src.ip"]
+        """
+        null = None
+        params = {
+            "filter": {
+                "select": fields,
+                "where": f"{filter}",
+                "orderBy": [
+                    {
+                        "field": "time",
+                        "sortOrder": "ascending"
+                    }
+                ],
+                "groupBy": groupBy,
+                "aggregateBy": aggregateBy,
+                "distributeBy": [],
+                "top": null,
+                "aliases": {
+                    "groupBy": {},
+                    "aggregateBy": null,
+                    "select": null
+                }
+            },
+            "groupValues": [],
+            "timeFrom": time_from,
+            "timeTo": time_to
+        }
+        api_url = self.__api_events_aggregate.format(limit, offset)
+        url = "https://{}{}".format(self.__core_hostname, api_url)
+
+        rq = exec_request(self.__core_session, url, method="POST", json=params)
+        response = rq.json()
+
+        if response is None or "rows" not in response:
+            self.log.error('status=failed, action=get_events_by_filter_aggregation, msg="Core data request return None or '
+                           'has wrong response structure", '
+                           'hostname="{}"'.format(self.__core_hostname))
+            raise Exception("Core data request return None or has wrong response structure")
+        return response.get("rows")
+
 
     def get_events_for_incident(self, fields, incident_id, time_from, time_to, limit, offset):
         """
